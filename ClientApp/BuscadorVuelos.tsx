99cb86e675dec900c39324a04ae8370c7127d693
UnRandomUrielXD <urielfermin.aguilarmendoza.gen23@cbtis105.edu.mx>
2025-05-21 10:18:34 -0600

    thebrdstbnrtnrhes

--- ClientApp/BuscadorVuelos.tsx
+++ ClientApp/BuscadorVuelos.tsx
@@ -9,11 +9,32 @@ type EstatusVuelo = {
     Nombre: string
 }
 
+export type Vuelo = {
+    PaisOrigen: string,
+    CiudadOrigen: string,
+    AeropuertoOrigen: string,
+    PaisDestino: string,
+    CiudadDestino: string,
+    AeropuertoDestino: string,
+    PasajerosActuales: number,
+    NombrePiloto: string,
+    FechaHoraSalida: string,
+    FechaHoraLlegadaAproximada: string,
+    EstatusVuelo: string
+}
+
 const BuscadorVuelos = () => {
 
     const [ciudadesOrigen, setCiudadesOrigen] = useState<Ciudad[]>([]);
     const [ciudadesDestino, setCiudadesDestino] = useState<Ciudad[]>([]);
     const [listadoEstatus, setListaEstatus] = useState<EstatusVuelo[]>([]);
+    const [listadoVuelos, setListaVuelos] = useState<Vuelo[]>([]);
+
+    const [fechaInicial, setFechaInicial] = useState("");
+    const [fechaFinal, setFechaFinal] = useState("");
+    const [estatus, setEstatus ] = useState("");
+    const [origen, setOrigen ] = useState("");
+    const [destino, setDestino ] = useState("");
 
     const listarCiudadesOrigen = async () => {
         const response = await fetch("http://localhost:5000/api/vuelos/ciudades-origen");
@@ -22,12 +43,67 @@ const BuscadorVuelos = () => {
             let ciudades : Array<Ciudad> = [];
 
             arr.map((x: string) => ciudades.push({Nombre: x}));
+
             setCiudadesOrigen(ciudades);
+        }
+    }
+
+    const listarCiudadesDestino = async () => {
+        const response = await fetch("http://localhost:5000/api/vuelos/ciudades-destino");
+        if(response.ok) {
+            const arr = await response.json();
+            let ciudades : Array<Ciudad> = [];
+
+            arr.map((x: string) => ciudades.push({Nombre: x}));
+
+            setCiudadesDestino(ciudades);
+        }
+    }
+
+    const listarEstatus = async () => {
+        const response = await fetch("http://localhost:5000/api/vuelos/estatus");
+        if(response.ok) {
+            const arr = await response.json();
+            let estatus : Array<EstatusVuelo> = [];
+
+            arr.map((x: string) => estatus.push({Nombre: x}));
+
+            setListaEstatus(estatus);
+        }
+    }
+
+    const listarVuelo = async () => {
+        let url = "http://localhost:5000/api/vuelos/listar-vuelos";
+        let parametros : string[] = [];
+
+        if(estatus){
+            parametros.push("estatus=" + estatus);
+        }
+
+        if(origen){
+            parametros.push("origen=" + origen);
+        }
+
+        if(destino){
+            parametros.push("destino=" + destino);
+        }
+
+        if(parametros.length > 0){
+            url += "?"
+            url += parametros.join("&");
+        }
+
+        const response = await fetch(url);
+        if(response.ok) {
+            const arr = await response.json();
+            setListaVuelos(arr);
         }
     }
 
     useEffect(()=> {
         listarCiudadesOrigen();
+        listarCiudadesDestino();
+        listarEstatus();
     },[]);
 
     return(
@@ -52,13 +128,8 @@ const BuscadorVuelos = () => {
                         <div className="col-sm-4">
                             <div className="mb-3">
                                 <label>Origen</label>
-                                <select className="form-control"></select>
-                            </div>
-                        </div>
-                        <div className="col-sm-4">
-                            <div className="mb-3">
-                                <label>Destino</label>
-                                <select className="form-control">
+                                <select className="form-control" onChange={(e)=> setOrigen(e.target.value)} value={origen}>
+                                    <option value="">(Todos)</option>
                                     {
                                         ciudadesOrigen.map(x => <option key={x.Nombre} value={x.Nombre}>{x.Nombre}</option>)
                                     }
@@ -67,12 +138,28 @@ const BuscadorVuelos = () => {
                         </div>
                         <div className="col-sm-4">
                             <div className="mb-3">
+                                <label>Destino</label>
+                                <select className="form-control" onChange={(e)=> setDestino(e.target.value)} value={destino}>
+                                    <option value="">(Todos)</option>
+                                    {
+                                        ciudadesDestino.map(x => <option key={x.Nombre} value={x.Nombre}>{x.Nombre}</option>)
+                                    }
+                                </select>
+                            </div>
+                        </div>
+                        <div className="col-sm-4">
+                            <div className="mb-3">
                                 <label>Estatus</label>
-                                <select className="form-control"></select>
+                                <select className="form-control" onChange={(e)=> setEstatus(e.target.value)} value={estatus}>
+                                    <option value="">(Todos)</option>
+                                    {
+                                        listadoEstatus.map(x => <option key={x.Nombre} value={x.Nombre}>{x.Nombre}</option>)
+                                    }
+                                </select>
                             </div>
                         </div>
                         <div className="col-12 d-flex justify-content-end">
-                            <button className="btn btn-primary">Buscar</button>
+                            <button className="btn btn-primary" onClick={()=> {listarVuelo();}}>Buscar</button>
                         </div>
                     </div>
                 </div>
@@ -81,7 +168,7 @@ const BuscadorVuelos = () => {
             <div className="card mt-4">
                 <div className="card-header">Vuelos encontrados</div>
                 <div className="card-body">
-                    <ListadoVuelos/>
+                    <ListadoVuelos vuelos={listadoVuelos}/>
                 </div>
             </div>
         </>
--- ClientApp/ListadoVuelos.tsx
+++ ClientApp/ListadoVuelos.tsx
@@ -1,5 +1,26 @@
-const ListadoVuelos = () => {
-    return (
+import { Vuelo } from "./BuscadorVuelos";
+
+interface Props  {
+    vuelos : Array<Vuelo>
+}
+
+const ListadoVuelos = ({vuelos}: Props) => {
+
+    const formatoFecha = (fecha: string) => {
+        const formato = new Intl.DateTimeFormat("es-MX", {day: "2-digit",month:"2-digit", year:"numeric"});
+        const dt = new Date(fecha);
+
+        return formato.format(dt);
+    }
+
+    const formatoHora = (fecha: string) => {
+        const formato = new Intl.DateTimeFormat("es-MX", {hour: "2-digit",minute:"2-digit"});
+        const dt = new Date(fecha);
+
+        return formato.format(dt);
+    }
+
+    return(
         <table className="table">
             <thead>
                 <tr>
@@ -12,8 +33,44 @@ const ListadoVuelos = () => {
                     <th>Estatus</th>
                 </tr>
             </thead>
+            <tbody>
+                {
+                    vuelos.length === 0 &&
+                    <tr>
+                        <td colSpan={7}>No se han encontrado vuelos</td>
+                    </tr>
+                }
+                {
+                    vuelos.length > 0 && 
+                    vuelos.map(x =>
+                    <tr>
+                        <td>
+                            <div>{formatoFecha(x.FechaHoraSalida)}</div>
+                            <div>{formatoHora(x.FechaHoraSalida)}</div>
+                        </td>
+                        <td>
+                            <div>{formatoFecha(x.FechaHoraLlegadaAproximada)}</div>
+                            <div>{formatoHora(x.FechaHoraLlegadaAproximada)}</div>
+                        </td>
+                        <td>
+                            <div>{x.PaisOrigen}</div>
+                            <div className="text-primary">{x.CiudadOrigen}</div>
+                            <div>{x.AeropuertoOrigen}</div>
+                        </td>
+                        <td>
+                            <div>{x.PaisDestino}</div>
+                            <div className="text-primary">{x.CiudadDestino}</div>
+                            <div>{x.AeropuertoDestino}</div>
+                        </td>
+                        <td>{x.PasajerosActuales}</td>
+                        <td>{x.NombrePiloto}</td>
+                        <td>{x.EstatusVuelo}</td>
+                    </tr>
+                    )
+                }
+            </tbody>
         </table>
-    );
+    )
 }
 
 export default ListadoVuelos;
